3bd35bcdf109750da26fd9e8ac6d7306687852de
Dines Poulsen <dinespoulsen84@gmail.com>
2016-12-13 11:30:47 +0000

    viewing all spaces

--- spec/features/viewing_spaces_spec.rb
+++ spec/features/viewing_spaces_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature "adding space" do
-
+feature "Users can view spaces" do
   before(:each) do
     User.create(email: 'test@test.com',
                 password: 'test')
   end
 
-  scenario "a user can add a space when signed in" do
+  scenario "all users should be able to view spaces" do
     sign_in
     add_space
+    visit("/spaces")
     expect(page).to have_content("test")
   end
 

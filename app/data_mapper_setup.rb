0c1b3a0e738647ebcb1e2fbb79b06099514c8da7
Tom Fuller <tomf95@hotmail.co.uk>
2016-12-13 09:41:39 +0000

    procfile + heroku setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-DataMapper.setup(:default, "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!

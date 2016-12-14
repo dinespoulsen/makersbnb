eed5e4de982b7997b952ed096d48865df0cc4424
Louisa <louisaspicer@gmail.com>
2016-12-14 21:58:26 +0000

    Fixed test error and added Rory's edits in

--- app/app.rb
+++ app/app.rb
@@ -104,6 +104,7 @@ class MakersBnb < Sinatra::Base
   get '/spaces/myincomingrequests' do
     @requests = current_user.spaces.requests
     @spaces = Space.all
+    @users = User.all
     erb(:'space/myincomingrequest')
   end
 
